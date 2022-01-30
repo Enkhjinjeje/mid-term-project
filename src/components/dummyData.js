35f0de02fd720eac4e2eb82985dd19e2d1f0d5a3
Enkhjinjeje <ekhjinjeje@gmail.com>
2022-01-29 23:26:47 -0800

    first mid project commit

--- src/components/dummyData.js
+++ src/components/dummyData.js
@@ -5,12 +5,13 @@ const dummyData = [
       title: "Weigthing class",
       location: "Y.M.C.A",
       image: "https://images.unsplash.com/photo-1547483238-f400e65ccd56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      time: "9am - 12pm"
+      time: "9am - 12pm",
     },
     {
       title: "Weigthing class",
       location: "Y.M.C.A",
-      image: "https://images.unsplash.com/photo-1547483238-f400e65ccd56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      image: "https://images.unsplash.com/photo-1547483238-f400e65ccd56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      time: "9am - 12pm",
     },
     {
       title: "Weigthing class",
